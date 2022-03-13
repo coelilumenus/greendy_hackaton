99f7e3a5206c3abcd94f27a6fb9749b37830cbf6
set <set>
2022-03-13 03:47:17 +0300

    Updates

--- assets/Home.5cfb2455.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.c6f5947c.js";import{o,c as r}from"./vendor.4ba7aea0.js";const c={};function t(n,a){return o(),r("div",null," home ")}var s=e(c,[["render",t]]);export{s as default};
--- assets/Information.ec4a02b2.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index.c6f5947c.js";import{o as r,c as e}from"./vendor.4ba7aea0.js";const n={};function t(c,a){return r(),e("div",null," information ")}var i=o(n,[["render",t]]);export{i as default};
