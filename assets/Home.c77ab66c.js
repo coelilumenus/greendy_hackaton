436896c2c33f79def216c6d792cad1d4036cc93d
set <set>
2022-03-13 04:10:52 +0300

    Updates

--- assets/Home.c77ab66c.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.78d96d4c.js";import{o,c as r}from"./vendor.ed21ff02.js";const c={};function t(n,a){return o(),r("div",null," home ")}var s=e(c,[["render",t]]);export{s as default};
--- assets/Profile.91782162.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.78d96d4c.js";import{o as r,c as o}from"./vendor.ed21ff02.js";const c={};function t(n,a){return r(),o("div",null," profile ")}var l=e(c,[["render",t]]);export{l as default};
